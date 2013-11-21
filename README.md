68d17be679d80e748cc0f306d1c66f0bdd3d7543
garrettdreyfus <garrettdreyfus@gmail.com>
2013-11-21 16:05:53 -0500

    added reading from a html file

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Libstripper
 ===========
 
-A simple python script to strip away css attributes that your not using. Hopefully js soon as well.
+A simple python script to strip away the css you're not using for cleanliness and speed.
+
+It writes to a new css file in the directory of your library. For example, library.css becomes library-stripped.css
--- libstripper.py
+++ libstripper.py
@@ -1,45 +1,91 @@
+from string import *
+import re
 #gets input from user
 print "Path to library to stripped."
 lib_path = str(raw_input())
-print "Path to text file of classes used."
+print "Path to your html file"
 classes_path = str(raw_input())
-classes = []
-output = open(lib_path[:-4]+"-stripped.css",'w')
-# get a list of things we want to preserve from the css file
-with open(classes_path) as ClassesFile:
-    for class_line in ClassesFile:
-        if class_line != '\n':
-            classes.append(class_line.replace("\n",''))
-# Get work done
-lib = open(lib_path).readlines()
-bracket_count=0
-initiated=False
-parsed_class = False
-bracket_watch = False
-for class_line in lib:
-    parsed_class = False
-    if initiated:
-        if '{' in class_line:
-            bracket_count+=class_line.count('{')
-            bracket_watch= True
-        if '}' in class_line:
-            bracket_count-=class_line.count('}')
-        if bracket_count>=0:
-            output.write(class_line)            
-        if bracket_watch == True and bracket_count==0:
-            initiated = False
-            bracket_watch == False
-    else:
-        for i in classes: 
-            if i in class_line: 
-                parsed_class = i
-        if parsed_class:
-            initiated = True
-            output.write(class_line)
-        if '{' in class_line:
-            bracket_count+=class_line.count('{')
-            bracket_watch= True
-        if '}' in class_line:
-            bracket_count-=class_line.count('}')
-        
-output.close()
+def strip_element(line):
+    match = re.search(r'<.*?\b\w+\b', line)
+    if match:
+        element = match.group()
+        element=element.replace('<','')
+        element = element.rstrip().lstrip()
+        return element
+    return None
+def strip_id(line):
+    match = re.search(r' id.*?=.*?\"(.+?)\"',line)
+    if match:
+        element = match.group()
+        element=element.replace(' id','').replace('=','').replace('"','').replace("'",'')
+        element = element.rstrip().lstrip()
+        return element
+    return None
+def strip_class(line):
+    match = re.search(r' class.*?=.*?\"(.+?)\"',line)
+    if match:
+        element = match.group()
+        element=element.replace(' class','').replace('=','').replace('"','').replace("'",'')
+        element = element.rstrip().lstrip()
+        return element
+    return None
+def strip_classes(classes_path):
+    classes=[]
+    class_file = open(classes_path).readlines()
+    for line in class_file:
+        if '<' in line:
+            element = strip_element(line)
+            if element not in classes: classes.append(element)
+            if 'class' in line:
+                stripped_class = strip_class(line)
+                if ' ' in stripped_class:
+                    stripped_class=stripped_class.split()
+                    for word in stripped_class: 
+                        if word not in classes: classes.append(word)
+                elif stripped_class not in classes: classes.append(stripped_class)
+            if 'id' in line:
+                stripped_id = strip_id(line)
+                if stripped_id not in classes: classes.append(stripped_id)
+    return classes
+def strip_library(lib_path,classes):
+    output = open(lib_path[:-4]+"-stripped.css",'w')
+    output.truncate()
+    lib = open(lib_path).readlines()
+    bracket_count=0 #balance of open and closed brackets
+    initiated=False #whether or not a key word has been found, this iniates reading into the ouput file
+    parsed_class = "" #the parsed class starts out as nothing
+    bracket_watch = False #whether or not a bracket has been detected, this is for cases where the first open bracket
+                          #is not on a seperate line
+    for class_line in lib: #iterate through the library
+        parsed_class = ""  #reset parsedclass
+        if initiated: #if a keyword has been found
+            #next two ifs add and detract from bracket balance 
+            if '{' in class_line:
+                bracket_count+=class_line.count('{')
+                bracket_watch= True #denotes that we have a found an open bracket
+            if '}' in class_line:
+                bracket_count-=class_line.count('}')
+            if bracket_count>=0: # if the brackets haven't been closed add it to the new file
+                output.write(class_line)            
+            if bracket_watch == True and bracket_count==0: #so if we have found an open bracket and the brackets are balanced we should stop writing to the file
+                initiated = False
+                bracket_watch == False
+        else:
+            for keyword in classes: #iterates through classes to see if a keywords in the line
+                if keyword:
+                    match = re.search(r"\b" + re.escape(keyword) + r"\b",class_line)
+                    if match:
+                        parsed_class = keyword
+            if parsed_class: # if there is a keyword we should initiate writing and write that line
+                initiated = True
+                output.write(class_line)
+            if '{' in class_line: # for cases like input{ where the bracket would otherwise not be detected
+                bracket_count+=class_line.count('{')
+                bracket_watch= True
+            if '}' in class_line:
+                bracket_count-=class_line.count('}')
+    output.close()
+    return
+classes= strip_classes(classes_path)
+print classes
+strip_library(lib_path,classes)
